99aa7632359d8efb92c46bc28d38463fba4b26d2
Caleb Nelson <ac117191@avcol.school.nz>
2022-03-18 14:15:32 +1300

    changed wording for line 3, added question 4, and rephrased the intro

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,12 +1,7 @@
-﻿/*Console.WriteLine(
-    FiggleFonts.Orge.Render("Hello World"));
-*/
-
-
-// Made by Caleb Nelson
+﻿// Made by Caleb Nelson
 Console.WriteLine("This is a Te Reo Maori word quiz.");
-Console.WriteLine("You only have one shot at each question so make it count");
-Console.WriteLine(" ");
+Console.WriteLine("There are 10 questions and you only have one shot at each one so make it count.");
+Console.WriteLine("The questions start off easy and get harder as the quiz goes on\n");
 Console.WriteLine("Question 1: What is the Maori word for family?");
 Console.WriteLine("A: Whanau");
 Console.WriteLine("B: Aotearoa");
@@ -23,7 +18,7 @@ else
 {
     Console.WriteLine("This is incorrect");
 }
-Console.WriteLine("Question 2: What is the Maori word for Love");
+Console.WriteLine("Question 2: What is the Maori word for Love?");
 Console.WriteLine("A: Awa");
 Console.WriteLine("B: Aroha");
 Console.WriteLine("C: Kai");
@@ -38,7 +33,7 @@ else
 {
     Console.WriteLine("This is incorrect");
 }
-Console.WriteLine("Question 3: What is the Maori word for Work ");
+Console.WriteLine("Question 3: What is the Maori word for Work?");
 Console.WriteLine("A: Mahi ");
 Console.WriteLine("B: Kiwi");
 Console.WriteLine("C: Marae");
@@ -46,6 +41,21 @@ Console.WriteLine("C: Marae");
 string q3 = Console.ReadLine();
 string upper_q3 = q3.ToUpper();
 if (upper_q3 == "A")
+{
+    Console.WriteLine("You are correct");
+}
+else
+{
+    Console.WriteLine("This is incorrect");
+}
+Console.WriteLine("Question 4: What is Maori for the word Hello?");
+Console.WriteLine("A: Mōrena");
+Console.WriteLine("B: Kapa Haka");
+Console.WriteLine("C: Kia ora");
+
+string q4 = Console.ReadLine();
+string upper_q4 = q4.ToUpper();
+if (upper_q4 == "C")
 {
     Console.WriteLine("You are correct");
 }
