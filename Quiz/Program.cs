bc8dbd85c508ff2d9836f656aa4a4859cce8ad4a
Caleb Nelson <ac117191@avcol.school.nz>
2022-03-18 13:34:38 +1300

    added string toUpper and changed if else if to if else.

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,15 +1,25 @@
-﻿Console.WriteLine("This is a Te Reo Maori word quiz.");
+﻿/*Console.WriteLine(
+    FiggleFonts.Orge.Render("Hello World"));
+*/
+
+
+// Made by Caleb Nelson
+Console.WriteLine("This is a Te Reo Maori word quiz.");
 Console.WriteLine("You only have one shot at each question so make it count");
+Console.WriteLine(" ");
 Console.WriteLine("Question 1: What is the Maori word for family?");
 Console.WriteLine("A: Whanau");
 Console.WriteLine("B: Aotearoa");
 Console.WriteLine("C: Haka");
-string q1 = Console.ReadLine();
-if (q1 == "A" || q1 == "a")
+
+// This makes it so the answer no matter what case it will make it capital
+string q1 = (Console.ReadLine());
+string upper_q1 = q1.ToUpper();
+if (upper_q1 == "A")
 {
     Console.WriteLine("You are correct");
 }
-else if (q1 == "B" || q1 == "C" || q1 == "b" || q1 == "c")
+else
 {
     Console.WriteLine("This is incorrect");
 }
@@ -17,12 +27,14 @@ Console.WriteLine("Question 2: What is the Maori word for Love");
 Console.WriteLine("A: Awa");
 Console.WriteLine("B: Aroha");
 Console.WriteLine("C: Kai");
+
 string q2 = Console.ReadLine();
-if (q2 == "B" || q2 == "b")
+string upper_q2 = q2.ToUpper();
+if (upper_q2 == "B")
 {
     Console.WriteLine("You are correct");
 }
-else if (q2 == "A" || q2 == "C" || q2 == "a" || q2 == "c")
+else 
 {
     Console.WriteLine("This is incorrect");
 }
@@ -30,12 +42,14 @@ Console.WriteLine("Question 3: What is the Maori word for Work ");
 Console.WriteLine("A: Mahi ");
 Console.WriteLine("B: Kiwi");
 Console.WriteLine("C: Marae");
+
 string q3 = Console.ReadLine();
-if (q3 == "A" || q3 == "a")
+string upper_q3 = q3.ToUpper();
+if (upper_q3 == "A")
 {
     Console.WriteLine("You are correct");
 }
-else if (q3 == "B" || q3 == "C" || q3 == "b" || q3 == "c")
+else
 {
     Console.WriteLine("This is incorrect");
 }
